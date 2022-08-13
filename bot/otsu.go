2b5fb34713d0dc2ced3e84a9e7b0188dff5f74f6
Mudkip <me@mudkip.pw>
2022-08-13 16:52:39 -0230

    fix: removed redundant values and better handling of transparency

--- bot/otsu.go
+++ bot/otsu.go
@@ -28,25 +28,22 @@ func Otsu(wand *imagick.MagickWand, args OtsuArgs) ([]*imagick.MagickWand, error
 		}
 
 		for _, pixel := range pixels {
-			gray := -1
+			var gray int
 			alpha := pixel.GetAlpha()
 			if (alpha != 1) {
 				if (alpha == 0) {
 					pixel.SetColor("#FFFFFF");
-					gray = 1
+					gray = 255
 				}
 				pixel.SetAlpha(1);
 			}
-			if (gray != 1) {
+			if (gray != 255) {
 				red   := pixel.GetRed() * 255
 				green := pixel.GetGreen() * 255
 				blue  := pixel.GetBlue() * 255
 				gray = int(0.2126 * red + 0.7152 * green + 0.0722 * blue)
 				hex := fmt.Sprintf("#%02x%02x%02x", gray, gray, gray)
 				pixel.SetColor(hex)
-			}
-			if (gray == -1) {
-				continue
 			}
 			histogram[gray] += 1
 			numOfPixels++
