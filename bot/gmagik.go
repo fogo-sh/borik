8e6d2deb52aa7fc97f911e3ffb3f5d6a7024d293
Riley Flynn <riley@rileyflynn.me>
2022-12-25 21:18:55 -0330

    Fix gmagik (#36)
    Resolves #34
    
    Fix issue preventing gmagik from correctly generating gifs, introduced in 7202806fc3c75755b69ce4b23cbfb2471eefa26a

--- bot/gmagik.go
+++ bot/gmagik.go
@@ -7,9 +7,11 @@ import (
 )
 
 type GmagikArgs struct {
-	ImageURL   string  `default:"" description:"URL to the image to process. Leave blank to automatically attempt to find an image."`
-	Scale      float64 `default:"1" description:"Scale of the magikification. Larger numbers produce more destroyed images."`
-	Iterations uint    `default:"5" description:"Number of iterations of magikification to run."`
+	ImageURL         string  `default:"" description:"URL to the image to process. Leave blank to automatically attempt to find an image."`
+	Scale            float64 `default:"1" description:"Scale of the magikification. Larger numbers produce more destroyed images."`
+	Iterations       uint    `default:"5" description:"Number of iterations of magikification to run."`
+	WidthMultiplier  float64 `default:"0.5" description:"Multiplier to apply to the width of the input image to produce the intermediary image."`
+	HeightMultiplier float64 `default:"0.5" description:"Multiplier to apply to the height of the input image to produce the intermediary image."`
 }
 
 func (args GmagikArgs) GetImageURL() string {
@@ -23,7 +25,14 @@ func Gmagik(wand *imagick.MagickWand, args GmagikArgs) ([]*imagick.MagickWand, e
 	lastFrame := wand
 
 	for i := uint(0); i < args.Iterations; i++ {
-		newFrame, err := Magik(lastFrame.Clone(), MagikArgs{Scale: args.Scale})
+		newFrame, err := Magik(
+			lastFrame.Clone(),
+			MagikArgs{
+				Scale:            args.Scale,
+				WidthMultiplier:  args.WidthMultiplier,
+				HeightMultiplier: args.HeightMultiplier,
+			},
+		)
 		if err != nil {
 			return nil, fmt.Errorf("error running magik: %w", err)
 		}
