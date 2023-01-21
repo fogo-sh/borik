64fd7b6cadb76be9abb936793e7f8d2e39d6a723
Riley Flynn <riley@rileyflynn.me>
2023-01-21 14:25:12 -0330

    Add arg description

--- bot/modulate.go
+++ bot/modulate.go
@@ -9,9 +9,9 @@ import (
 
 type ModulateArgs struct {
 	ImageURL   string  `default:"" description:"URL to the image to process. Leave blank to automatically attempt to find an image."`
-	Brightness float64 `default:"100"`
-	Saturation float64 `default:"100"`
-	Hue        float64 `default:"100"`
+	Brightness float64 `default:"100" description:"Percent change in brightness. Numbers > 100 increase brightness, < 100 decreases."`
+	Saturation float64 `default:"100" description:"Percent change in saturation. Numbers > 100 increase saturation, < 100 decreases."`
+	Hue        float64 `default:"100" description:"Percent change in hue. Numbers > 100 rotates hue clockwise, < 100 rotates counter-clockwise."`
 }
 
 func (args ModulateArgs) GetImageURL() string {
